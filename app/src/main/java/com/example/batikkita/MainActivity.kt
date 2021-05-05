36b8b33be1f585e314faf984a362700cce72ee34
Farlz <farrelarvino1@gmail.com>
2021-05-05 22:19:29 +0700

    splash screen

--- app/src/main/java/com/example/batikkita/MainActivity.kt
+++ app/src/main/java/com/example/batikkita/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.batikkita
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.ProgressBar
+import com.example.batikkita.databinding.ActivityMainBinding
+import com.github.ybq.android.spinkit.sprite.Sprite
+import com.github.ybq.android.spinkit.style.CubeGrid
+import com.github.ybq.android.spinkit.style.FoldingCube
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val foldingCube: Sprite = FoldingCube()
+        binding.loadingBar.setIndeterminateDrawable(foldingCube)
+
+        Handler(mainLooper).postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }, 1500L)
     }
 }
