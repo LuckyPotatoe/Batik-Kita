5ee6c191d0b55de82e9cbce368f6fd7a12ffda6e
Farlz <farrelarvino1@gmail.com>
2021-05-06 20:56:46 +0700

    set up toolbar and bottom nav finishing

--- app/src/main/java/com/example/batikkita/HomeActivity.kt
+++ app/src/main/java/com/example/batikkita/HomeActivity.kt
@@ -7,20 +7,24 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.batikkita.databinding.ActivityHomeBinding
+import com.example.batikkita.databinding.ActivityMainBinding
 
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_cart, R.id.navigation_gift, R.id.navigation_map, R.id.navigation_scan))
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
         navView.setupWithNavController(navController)
     }
 }
